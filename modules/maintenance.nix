adddf2e8c2450ec8fc3956fe7c6ee0317a52f8ad
ZachDavies <zdmalta@proton.me>
2025-03-09 03:53:48 +0100

    aesth

--- modules/maintenance.nix
+++ modules/maintenance.nix
@@ -1,17 +1,18 @@
-{ config, lib, pkgs, ... }:
+{ ... }:
 {
+
  # Auto Updates
  system.autoUpgrade = {
    enable = true;
    dates = "weekly";
    allowReboot = false;
  };
+
  # Auto Cleanup
  nix.gc.automatic = true;
  nix.gc.dates = "daily";
  nix.gc.options = "--delete-older-than 14d";
  nix.settings.auto-optimise-store = true;
-
 
 }
 
